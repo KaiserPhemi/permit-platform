7e1222da12c2425a692fc8033d124c0fb86e5f44
Oluwafemi Akinwa <andela-oakinwa@users.noreply.github.com>
2017-05-30 03:50:39 +0100

    chore(Index.jsx):add-index-component

---
+++ client/components/App.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+/**
+ * Defined as class component
+ */
+class App extends Component {
+    constructor(props) {
+
+    }
+    /**
+     * Handles redering to the DOM
+     * @return {Object}
+     */
+    render() {
+        return (
+            
+        );
+    }
+
+}
